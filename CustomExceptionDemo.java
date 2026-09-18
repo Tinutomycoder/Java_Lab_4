ce1b6ebe1581af364beee86b3f93316ab761b212
Tinu Tomy <119868835+Tinutomycoder@users.noreply.github.com>
2026-09-18 10:10:38 +0530

    Update CustomExceptionDemo.java

--- CustomExceptionDemo.java
+++ CustomExceptionDemo.java
@@ -1,10 +1,4 @@
-/*
- * Name: N V Edwin
- * Register No: 25PMC143
- * College: Marian College Kuttikanam (Autonomous)
- * Department: MCA
- */
-// Level 4 – User-Defined Exceptions: 16. Invalid Age Exception
+
 class InvalidAgeException extends Exception {
     public InvalidAgeException(String message) {
         super(message);
