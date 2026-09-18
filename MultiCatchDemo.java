18c3e81dc43e5c3d5379dd8c5cc0b5e1b0ea8ddf
Tinu Tomy <119868835+Tinutomycoder@users.noreply.github.com>
2026-09-18 10:13:28 +0530

    Clean up MultiCatchDemo.java header comments
    Removed header comments and unnecessary lines.

--- MultiCatchDemo.java
+++ MultiCatchDemo.java
@@ -1,10 +1,4 @@
-/*
- * Name: N V Edwin
- * Register No: 25PMC143
- * College: Marian College Kuttikanam (Autonomous)
- * Department: MCA
- */
-// Level 1 – Built-in Exceptions: 4. Multiple Exception Handling
+
 import java.util.Scanner;
 
 public class MultiCatchDemo {
