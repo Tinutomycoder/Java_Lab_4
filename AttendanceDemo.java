9af923a21fd93fe8aa584099fcfb809f8a04d042
Tinu Tomy <119868835+Tinutomycoder@users.noreply.github.com>
2026-09-18 10:08:48 +0530

    Clean up AttendanceDemo.java
    Removed comments containing personal information and added a new line.

--- AttendanceDemo.java
+++ AttendanceDemo.java
@@ -1,10 +1,4 @@
-/*
- * Name: N V Edwin
- * Register No: 25PMC143
- * College: Marian College Kuttikanam (Autonomous)
- * Department: MCA
- */
-// Level 4 – User-Defined Exceptions: 18. Low Attendance Exception
+
 import java.util.Scanner;
 
 class LowAttendanceException extends RuntimeException {
